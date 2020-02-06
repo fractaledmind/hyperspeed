16c72cc3cc6abb1babfc24ea12b1108caeb991c0
fractaledmind <stephen.margheim@gmail.com>
2020-02-06 11:23:07 +0100

    Get some basic specs up and running

--- lib/hyperspeed.rb
+++ lib/hyperspeed.rb
@@ -6,9 +6,9 @@ require_relative './hyperspeed/version'
 module Hyperspeed
   class Error < StandardError; end
 
-  def self.define # &block
+  def self.define(&block)
     definition_proxy = DefinitionProxy.new
-    definition_proxy.evaluate(&Proc.new)
+    definition_proxy.evaluate(&block)
   end
 
   # rubocop:disable Metrics/AbcSize
@@ -16,7 +16,7 @@ module Hyperspeed
     ast ||= define(&Proc.new)
 
     if ast[:type] == :ELEMENT
-      tag_name = ast[:tag].to_s.downcase || 'div'
+      tag_name = ast[:tag].to_s.downcase
       # ensure the string we are reducing isn't frozen, so that we can modify it
       content = ast[:children]&.reduce(+'') { |memo, child| memo << render(child) }
 
@@ -29,7 +29,7 @@ module Hyperspeed
         %(<#{tag_name}>#{content}</#{tag_name}>)
       end
     elsif ast[:type] == :TEXT
-      ast[:value]
+      ast[:value].to_s
     else
       fail 'Root `type` must be either ELEMENT or TEXT!'
     end
@@ -42,40 +42,54 @@ module Hyperspeed
       instance_eval(&block)
     end
 
+    # rubocop:disable Style/MethodMissingSuper
     def method_missing(tag_or_method, *args)
       if @self_before_instance_eval.respond_to?(tag_or_method, _include_private_methods = true)
-        @self_before_instance_eval.send(tag_or_method, *args, &Proc.new)
+        @self_before_instance_eval.send(tag_or_method, *args)
       else
         build_ast(tag_or_method, *args)
       end
-
-      super
     end
+    # rubocop:enable Style/MethodMissingSuper
 
     def respond_to_missing?(_tag_or_method, *_args)
       true
     end
 
-    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    def build_ast(tag_or_method, properties_or_children_or_text = nil, children_or_text = nil)
+    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
+    def build_ast(tag, properties_or_children_or_text = nil, children_or_text = nil)
       definition = {
         type: :ELEMENT,
-        tag: tag_or_method
+        tag: tag
       }
-      if children_or_text&.is_a?(Array)
-        definition.merge(properties: properties_or_children_or_text,
-                         children: children_or_text)
-      elsif children_or_text&.is_a?(String)
-        definition.merge(properties: properties_or_children_or_text,
-                         children: [{
-                           type: :TEXT,
-                           value: children_or_text
-                         }])
-      elsif properties_or_children_or_text&.is_a?(Hash)
+      if properties_or_children_or_text && children_or_text
+        if children_or_text.is_a?(Array)
+          unless properties_or_children_or_text.is_a?(Hash)
+            fail Error, 'when `children` are defined as second argument, first argument must be a `properties` Hash'
+          end
+
+          definition.merge(properties: properties_or_children_or_text,
+                           children: children_or_text)
+        elsif children_or_text.is_a?(String)
+          unless properties_or_children_or_text.is_a?(Hash)
+            fail Error, 'when `text` is defined as second argument, first argument must be a `properties` Hash'
+          end
+
+          definition.merge(properties: properties_or_children_or_text,
+                           children: [{
+                             type: :TEXT,
+                             value: children_or_text
+                           }])
+        else
+          # rubocop:disable Layout/LineLength
+          fail Error, 'when first argument is a `properties` Hash, second argument must be either `Array` of `children` or a `String`'
+          # rubocop:enable Layout/LineLength
+        end
+      elsif properties_or_children_or_text.is_a?(Hash)
         definition.merge(properties: properties_or_children_or_text)
-      elsif properties_or_children_or_text&.is_a?(Array)
+      elsif properties_or_children_or_text.is_a?(Array)
         definition.merge(children: properties_or_children_or_text)
-      elsif properties_or_children_or_text&.is_a?(String)
+      elsif properties_or_children_or_text.is_a?(String)
         definition.merge(children: [{
                            type: :TEXT,
                            value: properties_or_children_or_text
@@ -83,10 +97,10 @@ module Hyperspeed
       elsif properties_or_children_or_text.nil?
         definition
       else
-        fail "second argument must be either `Hash`, `Array`, or `String`, not #{properties_or_children_or_text.class}"
+        fail Error, 'first argument must be a `properties` Hash, `children` Array, text `String`, or empty'
       end
     end
-    # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
   end
 end
 
